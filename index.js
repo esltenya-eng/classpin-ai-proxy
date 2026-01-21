6074d183988a8d281b7eb5e842ab74d311499a94
esltenya-eng <esl.tenya@gmail.com>
2026-01-21 14:14:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post("/ai/text", async (req, res) => {
 
     // Node 20은 fetch 내장
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`,
+     `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro:generateContent?key=${GEMINI_API_KEY}`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
